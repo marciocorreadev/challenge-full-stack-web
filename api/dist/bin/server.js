18fb4ba561a7cb2a865d0c6a8518112a4d21f167
Márcio Corrêa <msc@outlook.com.br>
2021-04-18 21:25:52 -0300

    Postgress database added

--- api/dist/bin/server.js
+++ api/dist/bin/server.js
@@ -1,5 +1,9 @@
 "use strict";
 
+require("reflect-metadata");
+
+var _database = _interopRequireDefault(require("../database"));
+
 var _app = _interopRequireDefault(require("../app"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -7,6 +11,9 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 class Server {
   static async start(port) {
     try {
+      const connection = await (0, _database.default)();
+      if (connection.isConnected) console.log('Database connected!');else throw new Error("Failed to connect to the database!");
+
       _app.default.listen(port, () => console.log(`Server running at ${port}`));
     } catch (error) {
       console.log(error);
--- api/src/bin/server.ts
+++ api/src/bin/server.ts
@@ -1,8 +1,14 @@
+import "reflect-metadata";
+import createConnection from '@database'
 import app from '@app'
 
 class Server {
     static async start(port: number): Promise<void> {
         try {
+            const connection = await createConnection()
+            if(connection.isConnected) console.log('Database connected!');
+            else throw new Error("Failed to connect to the database!");
+            
             app.listen(port, () => console.log(`Server running at ${port}`))
         } catch (error) {
             console.log(error)
