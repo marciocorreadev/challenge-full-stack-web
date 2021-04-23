ee77cfc1fac8630e9f66c5ca36a48af795269cdd
Márcio Corrêa <msc@outlook.com.br>
2021-04-23 02:25:45 -0300

    Final Adjustments

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,10 +2,11 @@ import express from 'express'
 import 'express-async-errors'
 import routes from '@routes'
 import ErrorMiddleware from '@errors/index'
-
+import cors from 'cors'
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 app.use(ErrorMiddleware)
 
